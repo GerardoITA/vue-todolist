d4aacd00876c158139de0324dbe7262a1ed075cc
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-29 20:32:47 +0100

    4

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ createApp({
             error: false,
             inputText: ``,
             inputObject: {
-                text: `${this.inputText}`, done: false
+                text: this.inputText, done: false
             },
             circle: `fa-regular fa-circle`,
             circleCheck: `fa-regular fa-circle-check`,
             tasks: [
-                { text: 'Fare i compiti', done: false },
+                { text: 'Fare i compiti', done: true },
                 { text: 'Fare la spesa', done: true },
                 { text: 'Fare il bucato', done: false }
             ]
@@ -21,12 +21,22 @@ createApp({
     methods: {
         addTask(){
             this.tasks.push(this.inputObject);
-            console.log(this.tasks)
+            console.log(this.inputText);
+            console.log(this.tasks);
+            this.inputText = ``;    
         },
         removeTask(indice){
             this.tasks.splice(indice, 1)
-        }
-
+        },
+       changeTaskStatus(indice){
+            if (this.tasks[indice].done = false){
+                this.tasks[indice].done = true
+                console.log(this.tasks[indice].done)
+            } else {
+                this.tasks[indice].done = false
+                console.log(this.tasks[indice].done)
+            }
+        } 
     }
 }).mount(`#app`)
 
